abb0b1a06f33a01f998ace37f090d30747427ce1
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-01-28 12:03:59 +0100

    [UP]
    Mise à jour du projet en fin de matinée le 28/01/2022, intégration de la maquette desktop finie, commencement de l'intégration de la maquette tablette.

---
+++ CSS/app.js
@@ -0,0 +1,17 @@
+function algoDeCon() {
+   
+    arguments[0] = '10'
+    arguments[1] = '85'
+    arguments[2] = '63'
+    arguments[3] = '4'
+    arguments[4] = '51'
+    console.log(arguments[0]);
+    console.log(arguments[1]);
+    console.log(arguments[2]);
+    console.log(arguments[3]);
+    console.log(arguments[4]);
+    console.log(parseInt(arguments[1]) + parseInt(arguments[0])+ parseInt(arguments[2])+ parseInt(arguments[3])+ parseInt(arguments[4]))
+}
+
+algoDeCon(1, 2, 3, 4, 5)
+
