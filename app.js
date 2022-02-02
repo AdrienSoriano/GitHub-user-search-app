96bc2e2d2e65c2c5f5cbb838372ab5d0d7b36cf6
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-02-02 17:01:51 +0100

    [UP]
    mise à jour des fichiers CSS, HTML et JS

--- app.js
+++ app.js
@@ -14,13 +14,42 @@ function API (userName){
   fetch(url)
   .then((response) =>
         response.json().then((data) => {
+          document.querySelector('.avatar').setAttribute('src', data.avatar_url)
           document.querySelector('.pseudo').innerHTML = data.name
-          document.querySelector('.identifiant').innerHTML = data.login
-          document.querySelector('.dateCreation').innerHTML = data.created_at
+          document.querySelector('.identifiant').innerHTML = `@`+data.login
+          
+          const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+          let date = new Date(data.created_at)
+          let year = date.getFullYear();
+          let day = date.getDate()
+          document.querySelector('.dateCreation').innerHTML = "Joined" +" "+ day+" "+ month[date.getMonth()] + " "+ year
           document.querySelector('.biographie').innerHTML = data.bio
+          if(data.bio == null){
+            document.querySelector('.biographie').innerHTML = 'This profil has no bio.'
+          }
+          document.querySelector('.repos').innerHTML = data.public_repos
+          document.querySelector('.followers').innerHTML = data.followers
+          document.querySelector('.following').innerHTML = data.following
           document.querySelector('.localisation').innerHTML = data.location
+          document.querySelector('.twitter').innerHTML = data.twitter_username
+          if (data.twitter == null){
+            document.querySelector('.twitter').innerHTML = 'Not available'
+          }
+          document.querySelector('.web').innerHTML = data.blog
+          document.querySelector('.building').innerHTML = data.company
 
-  console.log(data)
 }))}
 
 API('octocat')
+
+const search = document.querySelector('button')
+
+search.addEventListener('click', function User (userName){
+  
+  let input = document.querySelector('input')
+  let inpValue = input.value
+  
+
+      API('user')
+})
+
